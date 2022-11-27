72a15de6e29f10d022c8dc6b7ca47047d6ae3c27
M0hitverma <mohit41verma@gmail.com>
2022-11-27 23:38:14 +0530

    Updates

---
+++ precache-manifest.c438361e311412876d3da04c71efd382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd1652b6bfd215e6c17d",
+    "url": "/TextUtils/static/css/main.11731b7f.chunk.css"
+  },
+  {
+    "revision": "cd1652b6bfd215e6c17d",
+    "url": "/TextUtils/static/js/main.cd1652b6.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "05b54db4a4c3678b5054",
+    "url": "/TextUtils/static/js/runtime~main.05b54db4.js"
+  },
+  {
+    "revision": "d861bd8e96933f4789c72ebad3b68428",
+    "url": "/TextUtils/index.html"
+  }
+];
